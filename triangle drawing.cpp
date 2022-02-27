796d2cde3f9679f416e0f0f59507b9e4644400a2
Zonetto <as076053@gmail.com>
2022-02-27 18:28:58 +0300

    Initial commit

--- triangle drawing.cpp
+++ triangle drawing.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int row = 5;
-    for (int i = row; i > 0; --i) {
-        for (int j = row - i; j >= 1; --j)  // space
+    int row = 4;
+    for (int i = 0; i <row ; --i) {
+        for (int j = 1; j <= row - i; --j)  // space
             cout << " ";
         for (int j = 1; j < i * 2; j++)
             cout << "*";
