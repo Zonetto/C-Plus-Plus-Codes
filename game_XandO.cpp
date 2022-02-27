dd8790624440e487cc120ea061decc99dd28be0f
Zonetto <as076053@gmail.com>
2022-02-27 09:39:40 +0300

    first commit

--- game_XandO.cpp
+++ game_XandO.cpp
@@ -3,17 +3,15 @@ using namespace std;
 char Matrix[3][3] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 char Player = 'X';
 
-void Check (char pos){
+void Check(char pos) {
     char pos;
     cin >> pos;
-    if  (pos != '1' || pos != '1' || pos != '2' || pos != '3' || pos != '4' || pos != '5' || pos != '6' || pos != '7' || pos != '8' || pos != '9')
+    if (pos != '1' || pos != '1' || pos != '2' || pos != '3' || pos != '4' || pos != '5' || pos != '6' || pos != '7' || pos != '8' || pos != '9')
         cout << "pleca enter numper true\n";
-    return 0;
 
-    else{
+    else
         cout << "Choose Your position - player ( " << Player << " ) : \n";
-        play(pos);
-        }
+    play(pos);
 }
 void printMatrix() {
     for (int i = 0; i < 3; i++) {
@@ -29,16 +27,16 @@ void play(int pos) {
     // if (pos != '1' || pos != '1' || pos != '2' || pos != '3' || pos != '4' || pos != '5' || pos != '6' || pos != '7' || pos != '8' || pos != '9')
     //     cout << "pleca enter numper true\n";
     // else {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++) {
-                if (Matrix[i][j] == pos)
-                    Matrix[i][j] = Player;
-            }
-        if (Player == 'X')
-            Player = 'O';
-        else
-            Player = 'X';
-    }
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++) {
+            if (Matrix[i][j] == pos)
+                Matrix[i][j] = Player;
+        }
+    if (Player == 'X')
+        Player = 'O';
+    else
+        Player = 'X';
+}
 char Who() {
     int Xc = 0, Oc = 0, check = 0;
     for (int i = 0; i < 3; i++) {
@@ -68,7 +66,7 @@ char Who() {
         Xc = 0;
         Oc = 0;
     }
-    
+
     if (Matrix[0][0] == 'X' && Matrix[1][1] == 'X' && Matrix[2][2] == 'X')
         return 'X';
     else if (Matrix[0][0] == 'O' && Matrix[1][1] == 'O' && Matrix[2][2] == 'O')
